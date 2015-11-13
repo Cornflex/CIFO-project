91ccb1e39fcb853241de94bd88ac4e62342c8e09
Felix Last <mail@felixlast.de>
2015-11-13 11:11:30 +0100

    update benchmark to vary triangle numbers as well

--- src/cifo/core/Benchmark.java
+++ src/cifo/core/Benchmark.java
@@ -9,7 +9,7 @@ public class Benchmark {
 	public static void main(String args[]) {
 		int fraction = 1;
 		int denominator = 9;
-		ParameterSet[] fullSet = ParameterSet.generateParameterSets(Main.SearchMethods.GA, 100, 3, 1000);
+		ParameterSet[] fullSet = ParameterSet.generateParameterSets(Main.SearchMethods.GA, 2, 500);
 		parameterSets = ParameterSet.getPortion(fraction, denominator, fullSet);
 		System.out.println("Benchmark started. Testing " + parameterSets.length +" out of " + fullSet.length + " parameter combinations at fraction " + fraction + " of " + denominator);
 		Main.runInBenchmarkMode(1, parameterSets[0]);
@@ -50,18 +50,21 @@ public class Benchmark {
 			this.TOURNAMENT_SIZE = TOURNAMENT_SIZE;
 		}
 
-		public static ParameterSet[] generateParameterSets(Main.SearchMethods SEARCH_METHOD, int NUMBER_OF_TRIANGLES, int NUMBER_OF_RUNS, int NUMBER_OF_GENERATIONS) {
+		public static ParameterSet[] generateParameterSets(Main.SearchMethods SEARCH_METHOD, int NUMBER_OF_RUNS, int NUMBER_OF_GENERATIONS) {
 			int[] populationSizes = {10,25,75};
 			int[] neighborhoodSizes = {4};
 			double[] mutationProbablilities = {0.01, 0.05, 0.25};
 			int[] tournamentSizes = {2,3,6};
+			int[] triangleNumbers = {75,100,125};
 			
 			ArrayList<ParameterSet> parameterSet = new ArrayList<>();
 			for(int i = 0; i < populationSizes.length; i++) {
 				for(int j = 0; j < neighborhoodSizes.length; j++) {
 					for(int k = 0; k < mutationProbablilities.length; k++) {
 						for(int l = 0; l < tournamentSizes.length; l++) {
-							parameterSet.add(new ParameterSet(SEARCH_METHOD, NUMBER_OF_TRIANGLES, NUMBER_OF_RUNS, NUMBER_OF_GENERATIONS, populationSizes[i], neighborhoodSizes[j], mutationProbablilities[k], tournamentSizes[l]));
+							for(int m = 0; m < triangleNumbers.length; m++) {
+								parameterSet.add(new ParameterSet(SEARCH_METHOD, triangleNumbers[m], NUMBER_OF_RUNS, NUMBER_OF_GENERATIONS, populationSizes[i], neighborhoodSizes[j], mutationProbablilities[k], tournamentSizes[l]));
+							}
 						}
 					}
 				}
